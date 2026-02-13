a4b49d5e2b8bedfb69a68a0bfc4a3d73b3db3c7e
Devinandana <deviinandana2004@gmail.com>
2026-02-13 23:09:01 +0530

    tracktimepage

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,7 @@
 document.getElementById("backBtn").addEventListener("click", function () {
     window.location.href = "index/index.html";
 });
+
+document.getElementById("trackBtn").addEventListener("click", function () {
+    window.location.href = "tracktime.html";
+});
